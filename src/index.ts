e4359cde0bd59211d2b14a0c7c165c837e578786
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2026-04-05 10:07:01 +0530

    feat(08-03): register file command in CLI entry point
    - Import fileCommand from commands/file.js
    - Add program.addCommand(fileCommand) after ingestCommand
    - Full test suite: 336 tests passing, 0 regressions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { askCommand } from './commands/ask.js';
 import { searchCommand } from './commands/search.js';
 import { listCommand } from './commands/list.js';
 import { ingestCommand } from './commands/ingest.js';
+import { fileCommand } from './commands/file.js';
 
 const program = new Command();
 
@@ -25,6 +26,7 @@ program.addCommand(askCommand);
 program.addCommand(searchCommand);
 program.addCommand(listCommand);
 program.addCommand(ingestCommand);
+program.addCommand(fileCommand);
 
 program.parseAsync(process.argv).catch((err: Error) => {
   process.stderr.write(`Error: ${err.message}\n`);
